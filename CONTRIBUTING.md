336463d33455bda79cb133b6a05456f72c1af31a
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-06 18:25:26 +0100

    Add files via upload

---
+++ CONTRIBUTING.md
@@ -0,0 +1,16 @@
+# Jelloff Code Contribution
+
+We completely welcome your intent to contributing to the Jelloff PHP Framework. We are very open about the development of the Jelloff Framewrok. Before sending in a pull request, please ensure you read through the details below. It is a list of all relevant code requests information.
+
+## Current Code Requests 
+
+- Enhance the current query builder with 'join' function and rewrite and arrange database query builder class files better.
+- Implement the PHP DOM Renderer for ReactJS server-side rendering (https://reactjs/react-php-v8js)
+- Adding the command for make:component and/or redesigning the manner in which custom components will be created.
+- Implement a mature Database Migrations
+- Implement [Faker.ng](https://faker.abujadevmeetup.com/) command via the exposed API
+- Hotfixes for Bugs and Errors
+
+## Addendum
+
+If you feel that there are more ways to develop Jelloff further or include code for functionality that will bring ease to developers and/or extend the cuurent offerings of Jelloff framework, then, send a [mail](mailto:isocroft@gmail.com) and the community will look into it and open up a discussion with you.
---
+++ app.php
@@ -0,0 +1,90 @@
+<?php
+ 
+   /*!---------------------------------------------------------
+    ! 
+    ! In the begining, We have to boot up the {System} and then
+    ! load up all class files needed to get started  using the
+    ! Jelloff autoloader.
+    !
+    !
+    !
+    ! * Jelloff (c) Copyright 2016
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    -----------------------------------------------------------*/
+
+    require_once __DIR__ . '/system/boot.php'; 
+
+   /*!---------------------------------------------------------
+    ! 
+    ! Create the most important object in this framework. Here
+    ! we are initializing the core of the framework where all
+    ! functionality resides {$app}.
+    !
+    !
+    !
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    ----------------------------------------------------------*/
+
+    $app = new \Providers\Core\App;
+
+   /*!--------------------------------------------------------
+    ! 
+    ! Create the 2 necessary services we need to get our
+    ! application to function properly. The Database and
+    ! Environment settings are loaded and processed here.
+    !
+    !
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    --------------------------------------------------------*/
+
+    $app->installDBService(require __DIR__ . '/configs/db.php');
+    $app->installENVService(require __DIR__ . '/configs/env.php');
+
+   /*!------------------------------------------------------
+    ! 
+    ! It's now time to make all custom ENV variables available 
+    ! to every part of the application by exposing the {$env}
+    ! variable
+    !
+    !
+    !
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    --------------------------------------------------------*/
+    $env = $app->exposeEnvironment(basename(__DIR__));
+
+   /*!------------------------------------------------------
+    ! 
+    ! Instantiate all necessary components needed by the app
+    ! internals e.g Routes, Controllers, Models and Views
+    !
+    !
+    !
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    ! *
+    --------------------------------------------------------*/
+    $app->registerCoreComponents();
+
+?>
