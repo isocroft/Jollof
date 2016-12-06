96bdeac9067cb24c76f1b4b7e9d9340177c4fff0
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-06 18:42:08 +0100

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,18 +1,17 @@
 <?php
 
-class User extends Model /* implements AuthService */ {
+class User extends Model {
 
     protected $table = 'tbl_app_users';
 
-    protected $primaryKey = 'user_id';
+    protected $primaryKey = 'id';
 
     protected $relations = array(
-       'Persona' => 'persona_id',
-       'Partner' => 'partner_id'
+ 
     );
 
     public function __construct(){
-
+        
         parent::__construct();
     }    
 
