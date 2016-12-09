fdbabb86dc17ad4a304b8df8c12c36ef4330b65e
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-09 18:30:16 +0100

    Create Reporter.php

---
+++ system/extensions/apis/src/Jollof/ErrorReporter/Reporter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Jollof\ErrorReporter;
+
+/**
+ * Jollof (c) Copyright 2016
+ *
+ *
+ * @package    Jollof\ErrorReporter
+ * @version    0.9.9
+ * @author     Ifeora Okechukwu.
+ * @license    MIT License
+ * @copyright   Mobicent, Ltd.
+ */
+
+use \Comms;
+
+class Reporter {
+
+    /*
+     * @var Comms
+     */
+
+	protected $communicator;
+
+    /**
+     * Constructor
+     *
+     * @param Comms $communicator
+     */
+
+    public function __construct(Comms $communicator){
+
+    	$this->communicator = $communicator;
+  
+    }
+
+    /**
+     * Sends the specific error details to the destination server/sservice endpoint
+     *
+     * @param string $host
+     * @param array $descriptors
+     * @param callable $callback
+     * @return mixed $result
+     */
+
+    public function sendError($host, array $descriptors, callable $callback){
+
+    		if(!is_callable(($callback))){
+
+    			return NULL;
+    		}
+
+    		$result = $callback($this->communicator->curl($host, 80, $descriptors));
+
+            return $result;
+
+    }
+}
+
+
+?>
