79e64307c722a95cf19d2bb7cf75c5836d59c288
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-10 17:34:51 +0100

    Create DBAccessInterface.php

---
+++ system/base/contracts/policies/DB/DBAccessInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Contracts\Policies;
+
+interface DBAccessInterface {
+
+      public function get(\array $columns, \array $clauseProps, $conjunction);
+
+      public function set(\array $values);
+
+      public function let(\array $columns, $conjunction);
+
+      public function del(\array $columns);
+
+      public function rawGet($query, \array $columns, \array $clauseProps);
+
+}
+
+
+?>
