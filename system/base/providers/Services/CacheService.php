9837a2b906b2913a780710fd9ff75e01aa5ac22f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-14 16:09:39 +0100

    Create CacheService.php

---
+++ system/base/providers/Services/CacheService.php
@@ -0,0 +1,51 @@
+<?php
+
+/*!
+ * Jollof Framework (c) 2016
+ * 
+ * {CacheSerivce.php}
+ */
+
+namespace Providers\Services;
+
+use \Contracts\Policies\CacheAccessInterface as CacheAccessInterface;
+
+class CacheService implements CacheAccessInterface {
+
+
+      protected $memcache;
+
+      protected $isCacheAvialable;
+     
+      public function __construct($options){
+
+         try{
+
+      	     $this->memcache = new Memcache;
+
+      	     $this->isCacheAvialable = $this->memcache->connect($options['host'], $options['port']);
+
+         }catch(\Exception $e){
+
+              throw $e;
+         }
+      }
+
+      public function set($key, $val){
+
+           $this->memcache->set($key, $val);
+      }
+
+      public function get($key){
+
+           return $this->memcache->get($key); 
+      }
+
+      public function has($key){
+
+
+      }
+
+}
+
+?>
